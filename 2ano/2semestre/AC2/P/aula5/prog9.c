cf0720eebc7cb93334c1e3932b027a4826c4b661
CrazyBIRB <tiagojralmeida@outlook.com>
2024-03-22 11:30:27 +0000

    AC2 - Guiao 5 prog9 a funcionar

--- 2ano/2semestre/AC2/P/aula5/prog9.c
+++ 2ano/2semestre/AC2/P/aula5/prog9.c
@@ -1,13 +1,17 @@
 #include <detpic32.h>
 #include "/home/tiago/github/UA-LECI/2ano/2semestre/AC2/P/Functions.c"
 
+// Teoria:
+// A frequencia de refrescamento é de 100hz, ou seja, em 1 segundo são contadas 100 vezes
+// Ou seja, para o ctrl contar até 5 segundos, pode-se usar o refrescamento 500 vezes
+
 int main(void){
 
     // declare variables 
     unsigned int counter, i;
     unsigned char value;
     unsigned int k = 50;
-    unsigned int flag = 0;
+    unsigned int ctrl = 0;
 
     // configure RB8-RB14 as outputs
     TRISB = TRISB & 0x80FF; // (1000 0000 1111 1111)
@@ -32,13 +36,10 @@ int main(void){
                 value = toBcd(counter); 
                 send2displays(value); 
                 delay(10);
-                if(flag > 0 && flag < 100000000){
-                    flag+= 10;
-                }
-                else if(flag >= 100000000){
-                    flag = 0;
-                    LATCbits.LATC14 = 0;
-                }
+
+                //
+                if(ctrl > 0){ ctrl--; }
+                else{ LATCbits.LATC14 = 0; }
 
             } while(++i < k);
             
@@ -49,7 +50,13 @@ int main(void){
             else if(PORTBbits.RB0 == 1 && counter == 0){ 
                 counter = 59;
                 k = 50;
-                flag = 1;
+                ctrl = 500;
+                LATCbits.LATC14 = 1;
+            }
+            else if(PORTBbits.RB0 == 0 && counter == 59){ 
+                counter = 0;
+                k = 20;
+                ctrl = 500;
                 LATCbits.LATC14 = 1;
             }
             else{ 
