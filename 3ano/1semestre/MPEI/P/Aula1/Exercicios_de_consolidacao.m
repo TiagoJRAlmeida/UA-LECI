97f6dc8cf13b125c7f6814badbece6eb149c97de
Tiago <tiagojralmeida04@outlook.com>
2024-09-23 22:57:22 +0100

    MPEI - Começo do guião da aula 1 (incompleto)

---
+++ 3ano/1semestre/MPEI/P/Aula1/Exercicios_de_consolidacao.m
@@ -0,0 +1,24 @@
+% Ex 1.1.2 Exercicios de consolidação
+
+% (a)
+a=(4:2:100);
+
+% (b)
+b = (5:-1:-5);
+
+% (c)
+c = (0:0.01:1);
+
+% (d)
+d1 = rand(20,30);
+d2 = d1(10:15,9:12);
+
+% (e)
+e = -pi:pi/15:pi;
+
+% (f)
+% A expressão plot(x, sin(4*pi*x), tendo em conta x = e neste ficheiro,
+% devolve o grafico da função sin(4*pi*x), em que o dominio, isto é, os
+% valores de x, são os pertencentes á serie -pi:pi/15:pi
+
+
